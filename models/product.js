e38298600e0a4078dfb4f92a7e0253b1a7a5c6e9
nehoraiharush <101175556+nehoraiharush@users.noreply.github.com>
2022-05-24 13:39:46 +0300

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,11 @@
+import Sequelize from 'sequelize'
+import sequelize from '../database/database'
+import database from '../database/database'
+
+const Product = database.define('product', {
+
+
+
+});
+
+export default Product;
