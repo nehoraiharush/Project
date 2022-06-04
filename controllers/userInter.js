74d6eb76cc164b79ea6ef34dfd52728858e8cbc1
Im-Stivik <mogil.sharon@gmail.com>
2022-06-04 15:49:30 +0300

    fixed a bug
    if one or more of the expected firlds dont come with the request, the whole server shuts down.
    so i added a try catch mechanizem to prevent the crashing

--- controllers/userInter.js
+++ controllers/userInter.js
@@ -5,119 +5,160 @@ const router = express.Router();
 import jwt from 'jsonwebtoken';
 import users from '../models/users.js'
 
+//NOTE!!
+//I added a try catch to every function to fix bug, more on that in the commit
+
 router.post('/signUp', async (req, res) => {
-    const { fName, lName, userEmail, userPassword } = req.body;
-    console.log(userEmail);
-    const email_exist = await users.findAll({ where: { userEmail: userEmail } });
-    if (email_exist.length > 0) {
-        return res.status(201).json({
-            status: false,
-            message: 'User is already exist'
+    try{
+        const { fName, lName, userEmail, userPassword } = req.body;
+        console.log(userEmail);
+        const email_exist = await users.findAll({ where: { userEmail: userEmail } });
+        if (email_exist.length > 0) {
+            return res.status(201).json({
+                status: false,
+                message: 'User is already exist'
+            });
+        }
+
+        const passCode = Math.floor(Math.random() * 8999) + 1000;
+        const enPass = await bcrypt.hash(userPassword, 10)
+        users.create({
+            fName: fName,
+            lName: lName,
+            userEmail: userEmail,
+            userPassword: enPass,
+            userPasscode: passCode,
+            isAuthorized: false
+        });
+        return res.status(200).json({
+            message: "User added successfully"
         });
     }
-
-    const passCode = Math.floor(Math.random() * 8999) + 1000;
-    const enPass = await bcrypt.hash(userPassword, 10)
-    users.create({
-        fName: fName,
-        lName: lName,
-        userEmail: userEmail,
-        userPassword: enPass,
-        userPasscode: passCode,
-        isAuthorized: false
-    });
-    return res.status(200).json({
-        message: "User added successfully"
-    });
-
+    catch{
+        console.log('Error with the fields')
+        return res.status(201).json({
+            massage: "Error: the one field or more are broken or not existant"
+        })
+    }
 })
 
 router.post('/login', async (req, res) => {
-    const { userEmail, userPassword } = req.body;
-    const user_find = await users.findAll({ where: { userEmail: userEmail } });
-    if (user_find.length > 0) {
-        const user = user_find[0];
-        const isMatch = await bcrypt.compare(userPassword, user.userPassword);
-        if (isMatch) {
-            const data = {
-                first_Name: user.fName,
-                last_name: user.lName,
-                email: user.userEmail,
-                passcode: user.userPasscode,
-                data_password: user.password,
-                userId: user.id
-            };
+    try{
+        const { userEmail, userPassword } = req.body;
+        const user_find = await users.findAll({ where: { userEmail: userEmail } });
+        if (user_find.length > 0) {
+            const user = user_find[0];
+            const isMatch = await bcrypt.compare(userPassword, user.userPassword);
+            if (isMatch) {
+                const data = {
+                    first_Name: user.fName,
+                    last_name: user.lName,
+                    email: user.userEmail,
+                    passcode: user.userPasscode,
+                    data_password: user.password,
+                    userId: user.id
+                };
 
-            const token = jwt.sign(data, 'KYCLSHb37FUbQVx0mUM6lxmWS0yWUD7Z');
-            return res.status(200).json({
-                status: true,
-                token: token
+                const token = jwt.sign(data, 'KYCLSHb37FUbQVx0mUM6lxmWS0yWUD7Z');
+                return res.status(200).json({
+                    status: true,
+                    token: token
+                });
+            }
+        } else {
+            return res.status(201).json({
+                status: false,
+                message: 'User is not Exist'
             });
         }
-    } else {
+    }
+    catch{
+        console.log('Error with the fields')
         return res.status(201).json({
-            status: false,
-            message: 'User is not Exist'
-        });
+            massage: "Error: the one field or more are broken or not existant"
+        })
     }
 })
+
 //vierfying the user
 router.put('/verify', async (req, res) => {
-    const { userEmail, userPasscode } = req.body;
-    //serchig for user with the above details
-    const user = await users.findAll({ where: { userEmail: userEmail } && { userPasscode: userPasscode } });
-    if (user.length == 0) {
+    try{
+        const { userEmail, userPasscode } = req.body;
+        //serchig for user with the above details
+        const user = await users.findAll({ where: { userEmail: userEmail } && { userPasscode: userPasscode } });
+        if (user.length == 0) {
+            return res.status(201).json({
+                status: false,
+                message: 'User not found'
+            });
+        } else {
+            user[0].isAuthorized = true;
+            user[0].save();
+            return res.status(200).json({
+                status: true,
+                message: `${user[0].fName} ${user[0].lName} is authorized`
+            });
+        }
+    }
+    catch{
+        console.log('Error with the fields')
         return res.status(201).json({
-            status: false,
-            message: 'User not found'
-        });
-    } else {
-        user[0].isAuthorized = true;
-        user[0].save();
-        return res.status(200).json({
-            status: true,
-            message: `${user[0].fName} ${user[0].lName} is authorized`
-        });
+            massage: "Error: the one field or more are broken or not existant"
+        })
     }
 })
 //asking for a user email and sending a passcode
 router.put('/forgetPassword', async (req, res) => {
+        try{
+        const { userEmail } = req.body;
+        const user = await users.findAll({ where: { userEmail: userEmail } });
 
-    const { userEmail } = req.body;
-    const user = await users.findAll({ where: { userEmail: userEmail } });
+        if (user.length > 0) {//find a user
+            user[0].userPasscode = Math.floor(Math.random() * 8999) + 1000;//new passcode between 1000-9999
+            user[0].isAuthorized = false;//user not authorized antmore
 
-    if (user.length > 0) {//find a user
-        user[0].userPasscode = Math.floor(Math.random() * 8999) + 1000;//new passcode between 1000-9999
-        user[0].isAuthorized = false;//user not authorized antmore
+            user[0].save();
 
-        user[0].save();
-
-        return res.status(200).json({
-            status: true,
-            message: `Passcode ${user[0].userPasscode}`
-        });
-    } else {//didn't find
+            return res.status(200).json({
+                status: true,
+                message: `Passcode ${user[0].userPasscode}`
+            });
+        } else {//didn't find
+            return res.status(201).json({
+                status: false,
+                message: "User not exist"
+            })
+        }
+    }
+    catch{
+        console.log('Error with the fields')
         return res.status(201).json({
-            status: false,
-            message: "User not exist"
+            massage: "Error: the one field or more are broken or not existant"
         })
     }
-
 })
 router.put('changePassword', async (req, res) => {
-    const { entered_id, password } = req.body;
+        try{
+        const { entered_id, password } = req.body;
 
-    const user = await users.findAll({ where: { id: entered_id } })
-    if (user.length > 0) {
-        user[0].userPassword = bcrypt.hash(password);
-        user[0].save();
-        return res.status(200).json({
-            message: "Password updated successfully"
-        });
-    } else {
+        const user = await users.findAll({ where: { id: entered_id } })
+        if (user.length > 0) {
+            user[0].userPassword = bcrypt.hash(password);
+            user[0].save();
+            return res.status(200).json({
+                message: "Password updated successfully"
+            });
+        } else {
+            return res.status(201).json({
+                message: "User isn't exist"
+            });
+        }
+    }
+    catch{
+        console.log('Error with the fields')
         return res.status(201).json({
-            message: "User isn't exist"
-        });
+            massage: "Error: the one field or more are broken or not existant"
+        })
     }
 })
 
